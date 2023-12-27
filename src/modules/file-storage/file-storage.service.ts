ed8c19b755f17270c3a99bd4bd0e0e5485e0319d
Alexander S <oleksandr.slv@gmail.com>
2023-12-27 12:21:49 +0200

    refactor FileStorageService;

--- src/modules/file-storage/file-storage.service.ts
+++ src/modules/file-storage/file-storage.service.ts
@@ -6,16 +6,27 @@ import { ROOT_AUDIO_DIR } from './file-storage.constants';
 
 @Injectable()
 export class FileStorageService {
-  public save(buffer: Buffer, fileName: string) {
-    return fs.writeFile(`${ROOT_AUDIO_DIR}/${fileName}`, buffer);
+  public saveAudio(buffer: Buffer, fileName: string) {
+    return this.save(`${ROOT_AUDIO_DIR}/${fileName}`, buffer);
   }
 
-  public get(fileName: string): Promise<Buffer> {
-    return fs.readFile(`${ROOT_AUDIO_DIR}/${fileName}`);
+  public getAudio(fileName: string): Promise<Buffer> {
+    return this.read(`${ROOT_AUDIO_DIR}/${fileName}`);
   }
 
-  public async delete(fileName: string): Promise<void> {
-    const path = `${ROOT_AUDIO_DIR}/${fileName}`;
+  public async deleteAudio(fileName: string): Promise<void> {
+    return this.delete(`${ROOT_AUDIO_DIR}/${fileName}`);
+  }
+
+  private save(path: string, buffer: Buffer) {
+    return fs.writeFile(path, buffer);
+  }
+
+  private read(path: string): Promise<Buffer> {
+    return fs.readFile(path);
+  }
+
+  private async delete(path: string): Promise<void> {
     try {
       await fs.stat(path);
     } catch (error: any) {
